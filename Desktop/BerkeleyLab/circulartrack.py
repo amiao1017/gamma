1627c54d051e84e0e17ac46547d4144c9635df71
russellkunes <samkunes@MCB-SKUNES-MBP.local>
2016-08-10 13:08:37 -0400

    deleted commented out code

--- Desktop/BerkeleyLab/circulartrack.py
+++ Desktop/BerkeleyLab/circulartrack.py
@@ -12,13 +12,6 @@ def magnitude2(x):
 	g = (np.sum(a,axis=1))**0.5
 	return g
 
-#def disttoline(m,n,k): #m and n are the two points that define the line, k is the other point 
-#	r = m - k #subtract element wise
-#	v = m - n # vector 
-#	vperp = np.array([v[1],-v[0]])
-#	dotproduct = vperp.dot(r)
-#	distance  = abs(dotproduct / magnitude(vperp))
-#	return distance
 def disttoline(m,n,lat,lon):
 	v = m-n
 	vperp = np.array([v[1],-v[0]])
@@ -82,34 +75,6 @@ def pdistance(latandlon,points_a_through_d,lat,lon):
 	pdistancea[narray == 1] = magnitude2(latandlon[narray ==1] - c) + cd + ad
 	pdistancea[narray == 2] = magnitude2(latandlon[narray ==2] - d) + ad
 	pdistancea[narray == 3] = magnitude2(latandlon[narray ==3] - a)
-	#############
-	#indexab = np.squeeze(np.argwhere(narray ==0))
-	#indexbc = np.squeeze(np.argwhere(narray ==1))
-	#indexcd = np.squeeze(np.argwhere(narray ==2))
-	#indexad = np.squeeze(np.argwhere(narray ==3))
-	#ablatandlon = latandlon[indexab]
-	#bclatandlon = latandlon[indexbc]
-	#cdlatandlon = latandlon[indexcd]
-	#adlatandlon = latandlon[indexad]
-	###########
-
-	#pdistancea = np.append((magnitude2(adlatandlon-a)),pdistancea)
-	#pdistancea = np.append((magnitude2(cdlatandlon-d)+ad),pdistancea)
-	#pdistancea = np.append((magnitude2(bclatandlon-c)+cd+ad),pdistancea)
-	#pdistancea = np.append((magnitude2(ablatandlon-b)+bc+cd+ad),pdistancea)
-	
-
-#	for i in latandlon:
-#		array = [disttoline(a,b,i), disttoline(b,c,i), disttoline(c,d,i),disttoline(d,a,i)]
-#		index = np.argmin(array)
-#		if index == 0: 
-#			pdistance.append(magnitude((i-b))+bc+cd+ad)
-#		if index == 1:
-#			pdistance.append(magnitude((i-c))+cd+ad)
-#		if index == 2:
-#			pdistance.append(magnitude((i-d))+ad)
-#		if index == 3:	
-#			pdistance.append(magnitude((i-a)))
 	return np.array(pdistancea)
 
 
